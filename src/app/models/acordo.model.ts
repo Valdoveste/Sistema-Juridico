ef3ea0b806e950848ac5cac219853bbb6fcf5f21
Valdoveste <caio.valdoveste@outlook.com.br>
2023-05-19 14:23:11 -0300

    Int16Arrty to Number

--- src/app/models/acordo.model.ts
+++ src/app/models/acordo.model.ts
@@ -1,7 +1,9 @@
+import { Data } from "@angular/router";
+
 export interface Acordo {
-  ID_ACORDO: Int16Array;
-  ID_USUARIO: Int16Array;
-  DATA_TENTATIVA: String;
-  VALOR_TENTATIVA: Int16Array;
+  ID_ACORDO: Number;
+  ID_USUARIO: Number;
+  DATA_TENTATIVA: Data;
+  VALOR_TENTATIVA: Number;
   CODICOES_TENTATIVA: String;
 }
--- src/app/models/andamento.models.ts
+++ src/app/models/andamento.models.ts
@@ -1,10 +1,12 @@
+import { Data } from "@angular/router";
+
 export interface Andamento {
-  ID_ANDAMENTO: Int16Array;
-  ID_USUARIO: Int16Array;
+  ID_ANDAMENTO: Number;
+  ID_USUARIO: Number;
   TIPO_ANDAMENTO: String;
   ANDAMENTO: String;
   RESPONSAVEL: String;
-  DATA_ANDAMENTO: String;
-  DATA_CADASTRO: String;
+  DATA_ANDAMENTO: Data;
+  DATA_CADASTRO: Data;
   ANEXO: [];
 }
