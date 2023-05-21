7fee3cc071f24a3dd8dd707c6b886ca13ba3f0cf
Valdoveste <caio.valdoveste@outlook.com.br>
2023-05-21 00:02:28 -0300

    Renaming the models and adding new propertys

--- src/app/models/andamento.models.ts
+++ src/app/models/andamento.models.ts
@@ -4,7 +4,7 @@ export interface Andamento {
   ID_ANDAMENTO: Number;
   ID_USUARIO: Number;
   TIPO_ANDAMENTO: String;
-  ANDAMENTO: String;
+  INFO_ANDAMENTO: String;
   RESPONSAVEL: String;
   DATA_ANDAMENTO: Data;
   DATA_CADASTRO: Data;
