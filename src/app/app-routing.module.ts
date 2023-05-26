f1acc1a6604e5cf6020b7b32bce558cca29f6c2a
Valdoveste <caio.valdoveste@outlook.com.br>
2023-05-26 20:22:51 -0300

    better router names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProcessPanelComponent } from './components/processComponents/process-panel/process-panel.component';
-import { ProcessSectionComponent } from './components/processComponents/process-section/process-section.component';
+import { ProcessCreateComponent } from './components/processComponents/process-create/process-create.component';
 
 const routes: Routes = [
   {
-    path: '', component: ProcessPanelComponent
+    path: 'painel-processos', component: ProcessPanelComponent
   },
-  
   {
-    path: 'panel', component: ProcessSectionComponent
+    path: 'criar-processo', component: ProcessCreateComponent
   }
-
 ];
 
 @NgModule({
