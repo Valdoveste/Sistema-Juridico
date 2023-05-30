bf53e64e92116f07c8992585a65f31fffe6de123
Valdoveste <caio.valdoveste@outlook.com.br>
2023-05-30 17:51:10 -0300

    create update component

--- src/app/components/processComponents/process-create/process-create.component.ts
+++ src/app/components/processComponents/process-create/process-create.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ParteContraria } from 'src/app/models/PARTE_CONTRARIA.model';
 import { Processo } from 'src/app/models/PROCESSO.model';
 import { ProcessoService } from '../../../sevices/processo.service';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-process-create',
@@ -13,17 +12,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ProcessCreateComponent implements OnInit {
   createProcessForm!: FormGroup;
 
+  constructor(private ProcessoService: ProcessoService, private router: Router) { }
+
+
   createProcessResquest: Processo = {
     ID_PROCESSO: '',
     NUMERO_PROCESSO: '',
     STATUS: '',
     TIPO_DE_ACAO: '',
     AREA_DO_DIREITO: '',
-    EMPPRESA: '',
+    EMPRESA: '',
     EMPRESA_CNPJ: '',
     AMBITO: '',
-    PARTE_CONTRARIA: '',
-    PARTE_CONTRARIA_CNPJ: '',
     PROCESSO_ESTADO: '',
     PROCESSO_CIDADE: '',
     PROCESSO_PAIS: '',
@@ -35,55 +35,67 @@ export class ProcessCreateComponent implements OnInit {
     PATRONO_RESPONSAVEL: '',
     PATRONOS_ANTERIORES: '',
     TEXTO_DO_OBJETO: '',
-    VALOR_DO_PEDIDO: '',
+    VALOR_DO_PEDIDO: 0,
     VALOR_INSTANCIA1: 0,
     VALOR_INSTANCIA2: 0,
     VALOR_INSTANCIA3: 0,
     VALOR_INSTANCIA_EXTRAORDINARIA: 0,
-    DATA_CADASTRO_PROCESSO: '',
-    DATA_ULITMO_PROCESSO: '',
+    DATA_CADASTRO_PROCESSO: new Date,
     DATA_ULTIMO_ANDAMENTO: '',
     DATA_ENCERRAMENTO: '',
     MOTIVO_ENCERRAMENTO: '',
-    MOTIVO_BAIXA_PROVISORIA: ''
+    MOTIVO_BAIXA_PROVISORIA: '',
+    PARTE_CONTRARIA_NOME: '',
+    PARTE_CONTRARIA_NOME_FANTASIA: '',
+    PARTE_CONTRARIA_CPF: '',
+    PARTE_CONTRARIA_CNPJ: '',
+    PARTE_CONTRARIA_RG: '',
+    PARTE_CONTRARIA_ENDERECO: '',
+    PARTE_CONTRARIA_CEP: '',
+    PARTE_CONTRARIA_NUMERO: 0,
+    PARTE_CONTRARIA_COMPLEMENTO: '',
+    PARTE_CONTRARIA_ESTADO: '',
+    PARTE_CONTRARIA_CIDADE: '',
+    PARTE_CONTRARIA_PAIS: '',
+    PARTE_CONTRARIA_OBSERVACAO: '',
+    PARTE_CONTRARIA_CARGO: '',
+    PARTE_CONTRARIA_DATA_ADMISSAO: new Date,
+    PARTE_CONTRARIA_DATA_DEMISSAO: new Date,
+    PARTE_CONTRARIA_ULTIMO_SALARIO: 0
   }
 
-  createOpposingpartyResquest: ParteContraria = {
-    ID_PARTECONTRARIA: '',
-    PF_PJ: 0,
-    NOME: '',
-    NOME_FANTASIA: '',
-    CPF: '',
-    CNPJ: '',
-    RG: '',
-    ENDERECO: '',
-    CEP: '',
-    NUMERO: '',
-    COMPLEMENTO: '',
-    ESTADO: '',
-    CIDADE: '',
-    PAIS: '',
-    OBSERVACAO: '',
-    CARGO: '',
-    DATA_ADMISSAO: '',
-    DATA_DEMISSAO: '',
-    ULTIMO_SALARIO: 0,
-  }
-
-  constructor(private ProcessoService: ProcessoService, private router: Router) { }
+  // createOpposingpartyResquest: ParteContraria = {
+  //   ID_PARTECONTRARIA: '',
+  //   PF_PJ: 0,
+  //   NOME: '',
+  //   NOME_FANTASIA: '',
+  //   CPF: '',
+  //   CNPJ: '',
+  //   RG: '',
+  //   ENDERECO: '',
+  //   CEP: '',
+  //   NUMERO: '',
+  //   COMPLEMENTO: '',
+  //   ESTADO: '',
+  //   CIDADE: '',
+  //   PAIS: '',
+  //   OBSERVACAO: '',
+  //   CARGO: '',
+  //   DATA_ADMISSAO: '',
+  //   DATA_DEMISSAO: '',
+  //   ULTIMO_SALARIO: 0,
+  // }
 
   ngOnInit(): void {
     this.createProcessForm = new FormGroup({
-      ID_PROCESSO: new FormControl(''),
+      // ID_PROCESSO: new FormControl(''),
       NUMERO_PROCESSO: new FormControl('', [Validators.required]),
+      EMPRESA: new FormControl('', [Validators.required]),
+      EMPRESA_CNPJ: new FormControl('', [Validators.required]),
       STATUS: new FormControl('', [Validators.required]),
       TIPO_DE_ACAO: new FormControl('', [Validators.required]),
       AREA_DO_DIREITO: new FormControl('', [Validators.required]),
-      EMPPRESA: new FormControl('', [Validators.required]),
-      EMPRESA_CNPJ: new FormControl('', [Validators.required]),
       AMBITO: new FormControl('', [Validators.required]),
-      PARTE_CONTRARIA: new FormControl('', [Validators.required]),
-      PARTE_CONTRARIA_CNPJ: new FormControl('', [Validators.required]),
       PROCESSO_ESTADO: new FormControl('', [Validators.required]),
       PROCESSO_CIDADE: new FormControl('', [Validators.required]),
       PROCESSO_PAIS: new FormControl('', [Validators.required]),
@@ -100,44 +112,55 @@ export class ProcessCreateComponent implements OnInit {
       VALOR_INSTANCIA2: new FormControl('', [Validators.required]),
       VALOR_INSTANCIA3: new FormControl('', [Validators.required]),
       VALOR_INSTANCIA_EXTRAORDINARIA: new FormControl('', [Validators.required]),
-      DATA_CADASTRO_PROCESSO: new FormControl('', [Validators.required]),
-      DATA_ULITMO_PROCESSO: new FormControl('', [Validators.required]),
-      DATA_ULTIMO_ANDAMENTO: new FormControl('', [Validators.required]),
-      DATA_ENCERRAMENTO: new FormControl('', [Validators.required]),
-      MOTIVO_ENCERRAMENTO: new FormControl('', [Validators.required]),
-      MOTIVO_BAIXA_PROVISORIA: new FormControl('', [Validators.required]),
-      PF_PJ: new FormControl('', [Validators.required]),
-      NOME: new FormControl('', [Validators.required]),
-      NOME_FANTASIA: new FormControl('', [Validators.required]),
-      CPF: new FormControl('', [Validators.required]),
-      CNPJ: new FormControl('', [Validators.required]),
-      RG: new FormControl('', [Validators.required]),
-      ENDERECO: new FormControl('', [Validators.required]),
-      CEP: new FormControl('', [Validators.required]),
-      NUMERO: new FormControl('', [Validators.required]),
-      COMPLEMENTO: new FormControl('', [Validators.required]),
-      ESTADO: new FormControl('', [Validators.required]),
-      PAIS: new FormControl('', [Validators.required]),
-      OBSERVACAO: new FormControl('', [Validators.required]),
-      CARGO: new FormControl('', [Validators.required]),
-      DATA_ADMISSAO: new FormControl('', [Validators.required]),
-      DATA_DEMISSAO: new FormControl('', [Validators.required]),
-      ULTIMO_SALARIO: new FormControl('', [Validators.required]),
+      DATA_CADASTRO_PROCESSO: new FormControl(''),
+      DATA_ULTIMO_ANDAMENTO: new FormControl(''),
+      DATA_ENCERRAMENTO: new FormControl(''),
+      MOTIVO_ENCERRAMENTO: new FormControl(''),
+      MOTIVO_BAIXA_PROVISORIA: new FormControl(''),
+      PARTE_CONTRARIA_NOME: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_NOME_FANTASIA: new FormControl(''),
+      PARTE_CONTRARIA_CPF: new FormControl(''),
+      PARTE_CONTRARIA_CNPJ: new FormControl(''),
+      PARTE_CONTRARIA_RG: new FormControl(''),
+      PARTE_CONTRARIA_ENDERECO: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_CEP: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_NUMERO: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_COMPLEMENTO: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_ESTADO: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_PAIS: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_OBSERVACAO: new FormControl(''),
+      PARTE_CONTRARIA_CIDADE: new FormControl('', [Validators.required]),
+      PARTE_CONTRARIA_CARGO: new FormControl(''),
+      PARTE_CONTRARIA_DATA_ADMISSAO: new FormControl(''),
+      PARTE_CONTRARIA_DATA_DEMISSAO: new FormControl(''),
+      PARTE_CONTRARIA_ULTIMO_SALARIO: new FormControl('')
     });
   }
 
-  get STATUS() {
-    return this.createProcessForm.get('STATUS')!;
-  }
+  // get STATUS() {
+  //   return this.createProcessForm.get('STATUS')!;
+  // }
+
+  // get NOME_FANTASIA() {
+  //   return this.createProcessForm.get('NOME_FANTASIA')!;
+  // }
 
   createProcess() {
+    console.log(this.createProcessForm.errors)
     if (this.createProcessForm.valid) {
+      console.log("Valido")
       this.ProcessoService.createProcess(this.createProcessResquest)
         .subscribe({
           next: (process) => {
             this.router.navigate(['painel-processos'])
+          },
+          error: (processError) => {
+            console.log(processError)
           }
         });
+    } else {
+      console.log("Nao Valido")
+
     }
 
     return;
