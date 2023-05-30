4675b808493f0d9c7971707eb512a1a251f1cd1e
Valdoveste <caio.valdoveste@outlook.com.br>
2023-05-30 17:42:39 -0300

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProcessPanelComponent } from './components/processComponents/process-panel/process-panel.component';
 import { ProcessCreateComponent } from './components/processComponents/process-create/process-create.component';
+import { ProcessAreaComponent } from './components/processComponents/process-area/process-area.component';
 
 const routes: Routes = [
   {
-    path: 'painel-processos', component: ProcessPanelComponent
+    path: 'painel-processos',
+    component: ProcessPanelComponent
   },
   {
-    path: 'criar-processo', component: ProcessCreateComponent
+    path: 'criar-processo',
+    component: ProcessCreateComponent
+  },
+  {
+    path: 'painel-processos/processo-detalhes/:id',
+    component: ProcessAreaComponent
   }
 ];
 
