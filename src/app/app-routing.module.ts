ae671bfe50477b452ab59a0ce9babc1d7459a2b3
Valdoveste <caio.valdoveste@outlook.com.br>
2023-06-09 09:17:20 -0300

    base route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProcessPanelComponent } from './components/processComponents/process-panel/process-panel.component';
 import { ProcessCreateComponent } from './components/processComponents/process-create/process-create.component';
 import { ProcessAreaComponent } from './components/processComponents/process-area/process-area.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+
   {
     path: 'painel-processos',
     component: ProcessPanelComponent
