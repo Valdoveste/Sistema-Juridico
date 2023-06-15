5a39acacc768f592647d0df10f35272a8342b624
Valdoveste <caio.valdoveste@outlook.com.br>
2023-06-15 03:01:40 -0300

    New components

---
+++ src/app/components/processComponents/process-area/process-area-previous-patrons/process-area-previous-patrons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process-area-previous-patrons',
+  templateUrl: './process-area-previous-patrons.component.html',
+  styleUrls: ['./process-area-previous-patrons.component.scss']
+})
+export class ProcessAreaPreviousPatronsComponent {
+
+}
