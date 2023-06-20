34fabcf135ff32fb83179f30368e479072e719a9
Valdoveste <caio.valdoveste@outlook.com.br>
2023-06-20 02:56:54 -0300

    Update API

--- SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Controllers/SistemaController.cs
+++ SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Controllers/SistemaController.cs
@@ -17,46 +17,111 @@ namespace SistemaJuridicoWebAPI.Controllers
             _sistemaJuridicoDbContext = sistemaJuridicoDbContext;
         }
 
-        // GET
-
-        [HttpGet]
-        public async Task<IActionResult> GetAllProcess()
-        {
-
-            return Ok(await _sistemaJuridicoDbContext.PROCESSO.ToListAsync());
-        }
-
-        [HttpGet("Ambito")]
+        [HttpGet("ambito")]
         public async Task<IActionResult> GetAllAmbito()
         {
 
           return Ok(await _sistemaJuridicoDbContext.PROCESSO_AMBITO.ToListAsync());
         }
 
-        [HttpPost]
+        [HttpGet("area-do-direito")]
+        public async Task<IActionResult> GetAllAreaDoDireito()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_AREA_DO_DIREITO.ToListAsync());
+        }
+
+        [HttpGet("condicoes-tentativa-acordo")]
+        public async Task<IActionResult> GetAllCondicoesTentivaAcordo()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_CONDICOES_TENTATIVA_ACORDO.ToListAsync());
+        }
+
+
+        [HttpGet("fase")]
+        public async Task<IActionResult> GetAllFase()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_FASE.ToListAsync());
+        }
+
+        [HttpGet("foro-tribunal-orgao")]
+        public async Task<IActionResult> GetAllForoTribunalOrgao()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_FORO_TRIBUNAL_ORGAO.ToListAsync());
+        }
+
+        [HttpGet("motivo-do-encerramento")]
+        public async Task<IActionResult> GetAllMotivoDoEncerramento()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_MOTIVO_DO_ENCERRAMENTO.ToListAsync());
+        }
+
+        [HttpGet("patrono-responsavel")]
+        public async Task<IActionResult> GetAllPatronoResponsavel()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_PATRONO_RESPONSAVEL.ToListAsync());
+        }
+
+        [HttpGet("status")]
+        public async Task<IActionResult> GetAllStatus()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_STATUS.ToListAsync());
+        }
+
+
+        [HttpGet("tipo-de-acao")]
+        public async Task<IActionResult> GetAllTipoDeAcao()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_TIPO_DE_ACAO.ToListAsync());
+        }
+
+
+        [HttpGet("vara")]
+        public async Task<IActionResult> GetAllVara()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO_VARA.ToListAsync());
+        }
+
+
+        [HttpGet("processo")]
+        public async Task<IActionResult> GetAllProcess()
+        {
+
+          return Ok(await _sistemaJuridicoDbContext.PROCESSO.ToListAsync());
+        }
+
+
+        [HttpGet("processo/{id}")]
+        public async Task<IActionResult> GetProcess([FromRoute] Guid id)
+        {
+          var processo = await _sistemaJuridicoDbContext.PROCESSO.FirstOrDefaultAsync(x => x.ID_PROCESSO.Equals(id));
+          if (processo == null)
+          {
+            return NotFound();
+          }
+
+          return Ok(processo);
+
+        }
+
+        [HttpPost("add-processo")]
         public async Task<IActionResult> AddProcess([FromBody] PROCESSO processoRequest)
         {
-            processoRequest.ID_PROCESSO = Guid.NewGuid();
+          processoRequest.ID_PROCESSO = Guid.NewGuid();
 
-            await _sistemaJuridicoDbContext.PROCESSO.AddAsync(processoRequest);
+          await _sistemaJuridicoDbContext.PROCESSO.AddAsync(processoRequest);
 
-            await _sistemaJuridicoDbContext.SaveChangesAsync();
+          await _sistemaJuridicoDbContext.SaveChangesAsync();
 
-            return Ok(processoRequest);
+          return Ok(processoRequest);
         }
-
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetProcess([FromRoute]Guid id)
-        {
-          var processo = await  _sistemaJuridicoDbContext.PROCESSO.FirstOrDefaultAsync(x => x.ID_PROCESSO.Equals(id));
-            if(processo == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(processo);
-
-        }
-    }
+  }
 }
