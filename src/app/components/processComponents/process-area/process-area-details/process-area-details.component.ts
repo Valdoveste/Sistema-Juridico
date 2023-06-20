5d38d7d5a8b4f484d9798a83bb77ea1b00838c46
Valdoveste <caio.valdoveste@outlook.com.br>
2023-06-20 03:17:40 -0300

    Correcting route

--- src/app/components/processComponents/process-area/process-area-details/process-area-details.component.ts
+++ src/app/components/processComponents/process-area/process-area-details/process-area-details.component.ts
@@ -6,7 +6,4 @@ import { ProcessAreaComponent } from '../process-area.component';
   templateUrl: './process-area-details.component.html',
   styleUrls: ['./process-area-details.component.scss']
 })
-export class ProcessAreaDetailsComponent extends ProcessAreaComponent implements OnInit {
-
-    override ngOnInit(): void {  }
-}
+export class ProcessAreaDetailsComponent extends ProcessAreaComponent implements OnInit {}
