a4afb2f4819179b0900c34d856b1055b425a28c7
Valdoveste <caio.valdoveste@outlook.com.br>
2023-06-20 01:50:43 -0300

    sevice to services

--- src/app/components/processComponents/process-container/process-container.component.ts
+++ src/app/components/processComponents/process-container/process-container.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Processo } from 'src/app/models/PROCESSO.model';
-import { ProcessoService } from 'src/app/sevices/processo.service';
+import { ProcessoService } from 'src/app/services/processo.service';
 
 @Component({
   selector: 'app-process-container',
