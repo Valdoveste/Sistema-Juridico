e27dca819b8629be84d995c13124babecc1676f0
Valdoveste <caio.valdoveste@outlook.com.br>
2023-06-21 02:20:01 -0300

    Update

--- SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Models/USUARIO.cs
+++ SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Models/USUARIO.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Cryptography;
 
@@ -13,5 +13,7 @@ namespace SistemaJuridicoWebAPI.Models
         public string NOME_USUARIO { get; set; }
 
         public string SENHA { get; set; }
+
+        public int ACESSO_GESTAO { get; set; }
     }
 }
