4f63c094cee7d189eae55097533d4422d0af5499
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-01 10:31:29 -0300

    management routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { ProcessPanelComponent } from './components/processComponents/process-pa
 import { ProcessCreateComponent } from './components/processComponents/process-create/process-create.component';
 import { ProcessAreaComponent } from './components/processComponents/process-area/process-area.component';
 import { LoginComponent } from './components/login/login.component';
-import { ManagementComponent } from './components/management/management.component';
+import { MAmbitoComponent } from './components/management/components/m-ambito/m-ambito.component';
+import { MVaraComponent } from './components/management/components/m-vara/m-vara.component';
+import { MFaseComponent } from './components/management/components/m-fase/m-fase.component';
+import { MAreaDireitoComponent } from './components/management/components/m-area-direito/m-area-direito.component';
+import { MTipoAcaoComponent } from './components/management/components/m-tipo-acao/m-tipo-acao.component';
+import { MForoTribunalOrgaoComponent } from './components/management/components/m-foro-tribunal-orgao/m-foro-tribunal-orgao.component';
 
 const routes: Routes = [
   {
@@ -23,10 +28,36 @@ const routes: Routes = [
   {
     path: 'painel-processos/processo-detalhes/:id',
     component: ProcessAreaComponent
-  },
+  }
+  ,
   {
-    path: 'gestao',
-    component: ManagementComponent
+    path: 'gestao/ambito',
+    component: MAmbitoComponent
+  }
+  ,
+  {
+    path: 'gestao/vara',
+    component: MVaraComponent
+  }
+  ,
+  {
+    path: 'gestao/fase',
+    component: MFaseComponent
+  }
+  ,
+  {
+    path: 'gestao/area-do-direito',
+    component: MAreaDireitoComponent
+  }
+  ,
+  {
+    path: 'gestao/tipo-de-acao',
+    component: MTipoAcaoComponent
+  }
+  ,
+  {
+    path: 'gestao/foro-tribunal-orgao',
+    component: MForoTribunalOrgaoComponent
   }
 ];
 
