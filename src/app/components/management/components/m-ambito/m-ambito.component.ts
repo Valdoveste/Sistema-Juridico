bdae8d53ae9767d6a042b0ed858e365d38570e21
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-04 00:39:32 -0300

    Making an update in the component after the dialod were closed

--- src/app/components/management/components/m-ambito/m-ambito.component.ts
+++ src/app/components/management/components/m-ambito/m-ambito.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogAddAmbitoComponent } from './dialog-add-ambito/dialog-add-ambito.component';
 import { ProcessoAmbito } from 'src/app/models/PROCESSO_AMBITO.model';
 import { AmbitoService } from 'src/app/services/ambito.service';
+
+import { DialogAddAmbitoComponent } from './dialog-add-ambito/dialog-add-ambito.component';
 import { DialogDeleteAmbitoComponent } from './dialog-delete-ambito/dialog-delete-ambito.component';
 import { DialogEditAmbitoComponent } from './dialog-edit-ambito/dialog-edit-ambito.component';
 
@@ -20,11 +21,14 @@ export class MAmbitoComponent implements OnInit {
   ambitos: ProcessoAmbito[] = [];
 
   ngOnInit(): void {
+    this.loadAmbitos();
+  }
+
+  loadAmbitos() {
     this.AmbitoService.getAllAmbito()
       .subscribe({
         next: (ambitos: any) => {
           this.ambitos = ambitos;
-          console.log(ambitos)
         },
         error: (response: any) => {
           console.log(response)
@@ -32,37 +36,36 @@ export class MAmbitoComponent implements OnInit {
       })
   }
 
-
-  alteracaoEvent: EventEmitter<any> = new EventEmitter<any>();
-
-  fazerAlteracao() {
-    // Realize a alteração aqui
-
-    // Emita o evento para notificar outros componentes
-    console.log(this.alteracaoEvent.emit());
-  }
-
   openDialogAdd(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(DialogAddAmbitoComponent, {
+    const dialogRefAdd = this.dialog.open(DialogAddAmbitoComponent, {
       width: '350px',
       enterAnimationDuration,
       exitAnimationDuration,
     });
-  }
-  
-  openDialogRemove(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(DialogDeleteAmbitoComponent, {
-      width: '350px',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
+
+    dialogRefAdd.afterClosed().subscribe(result => { result ? this.loadAmbitos() : null; })
   }
 
-  openDialogEdit(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(DialogEditAmbitoComponent, {
+
+  openDialogRemove(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
+    const dialogRefRemove = this.dialog.open(DialogDeleteAmbitoComponent, {
       width: '350px',
+      data: { value: (event.currentTarget as HTMLElement).previousElementSibling?.previousElementSibling?.innerHTML, id: id },
       enterAnimationDuration,
       exitAnimationDuration,
     });
+
+    dialogRefRemove.afterClosed().subscribe(result => { result ? this.loadAmbitos() : null; })
+  }
+
+  openDialogEdit(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
+    const dialogRefEdit = this.dialog.open(DialogEditAmbitoComponent, {
+      width: '350px',
+      data: { value: (event.currentTarget as HTMLElement).previousElementSibling?.innerHTML, id: id },
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+    dialogRefEdit.afterClosed().subscribe(result => { result ? this.loadAmbitos() : null; })
   }
 }
