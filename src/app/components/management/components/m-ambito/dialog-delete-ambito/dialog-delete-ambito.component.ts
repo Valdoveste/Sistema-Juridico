5c5174f6e0cc7b607c7e004aebfb136466f2a9cf
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-04 00:39:06 -0300

    Edit,Remove & Add componnets and Ts done

--- src/app/components/management/components/m-ambito/dialog-delete-ambito/dialog-delete-ambito.component.ts
+++ src/app/components/management/components/m-ambito/dialog-delete-ambito/dialog-delete-ambito.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ProcessoAmbito } from 'src/app/models/PROCESSO_AMBITO.model';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AmbitoService } from 'src/app/services/ambito.service';
 
 @Component({
   selector: 'app-dialog-delete-ambito',
@@ -7,5 +8,21 @@ import { ProcessoAmbito } from 'src/app/models/PROCESSO_AMBITO.model';
   styleUrls: ['./dialog-delete-ambito.component.scss']
 })
 export class DialogDeleteAmbitoComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public removeData: any,
+    private AmbitoService: AmbitoService,
+    public dialogRef: MatDialogRef<DialogDeleteAmbitoComponent>
+  ) { }
 
+  deleteAmbito() {
+    this.AmbitoService.deleteAmbito(this.removeData.id)
+      .subscribe({
+        next: (response) => {
+          this.dialogRef.close(true);
+        },
+        error: (response) => {
+          console.log(response)
+        }
+      });
+  }
 }
