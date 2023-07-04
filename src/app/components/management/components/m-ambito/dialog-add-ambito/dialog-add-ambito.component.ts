e122787777ec40acfd2c4aca14aa1a9649aa3ae1
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-04 00:39:06 -0300

    Edit,Remove & Add componnets and Ts done

--- src/app/components/management/components/m-ambito/dialog-add-ambito/dialog-add-ambito.component.ts
+++ src/app/components/management/components/m-ambito/dialog-add-ambito/dialog-add-ambito.component.ts
@@ -1,16 +1,20 @@
-import { ChangeDetectorRef, Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProcessoAmbito } from 'src/app/models/PROCESSO_AMBITO.model';
 import { AmbitoService } from 'src/app/services/ambito.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
-import { MAmbitoComponent } from '../m-ambito.component';
+
 @Component({
   selector: 'app-dialog-add-ambito',
   templateUrl: './dialog-add-ambito.component.html',
   styleUrls: ['./dialog-add-ambito.component.scss']
 })
 export class DialogAddAmbitoComponent implements OnInit {
+  constructor(
+    private AmbitoService: AmbitoService,
+    public dialogRef: MatDialogRef<DialogAddAmbitoComponent>,
+  ) { }
+
   createAmbitoForm!: FormGroup;
 
   createAmbitoRequest: ProcessoAmbito = {
@@ -18,39 +22,25 @@ export class DialogAddAmbitoComponent implements OnInit {
     AMBITO: ''
   }
 
-  constructor(
-    private AmbitoService: AmbitoService,
-    private router: Router,
-    public dialogRef: MatDialogRef<DialogAddAmbitoComponent>,
-    private cdr: ChangeDetectorRef
-  ) { }
-
   ngOnInit(): void {
     this.createAmbitoForm = new FormGroup({
-      // ID: new FormControl('',),
       AMBITO: new FormControl('', [Validators.required])
     });
   }
 
   createAmbito() {
-    console.log(this.createAmbitoForm.errors)
     if (this.createAmbitoForm.valid) {
-      console.log("Valido")
       this.AmbitoService.createAmbito(this.createAmbitoRequest)
         .subscribe({
-          next: (process) => {
-            this.dialogRef.close();
-            this.router.navigate(['gestao/ambito'])
+          next: (response) => {
+            this.dialogRef.close(true);
+            this.createAmbitoForm.reset();
           },
-          error: (processError) => {
-            console.log(processError)
+          error: (response) => {
+            console.log(response)
           }
         });
-    } else {
-      console.log("Nao Valido")
-
     }
-
     return;
   }
 }
