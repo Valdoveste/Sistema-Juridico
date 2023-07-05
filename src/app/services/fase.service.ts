2eebfc4ded8101c322adafc05a6add0ed43b6d18
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-05 00:14:06 -0300

    fase components

--- src/app/services/fase.service.ts
+++ src/app/services/fase.service.ts
@@ -14,6 +14,20 @@ export class FaseService {
   constructor(private http: HttpClient) { }
 
   getAllFase(): Observable<ProcessoFase[]> {
-    return this.http.get<ProcessoFase[]>(this.baseApiUrl + '/api/Sistema/ambito')
+    return this.http.get<ProcessoFase[]>(this.baseApiUrl + '/api/Sistema/fase');
+  }
+
+  createFase(createFaseRequest: ProcessoFase): Observable<ProcessoFase> {
+    delete createFaseRequest.ID;
+    return this.http.post<ProcessoFase>(this.baseApiUrl + '/api/Sistema/add-fase', createFaseRequest);
+  }
+
+  updateFase(id: string, updateFaseRequest: ProcessoFase): Observable<ProcessoFase> {
+    updateFaseRequest.ID = id;
+    return this.http.put<ProcessoFase>(this.baseApiUrl + '/api/Sistema/update-fase/' + id, updateFaseRequest);
+  }
+
+  deleteFase(id: string): Observable<ProcessoFase> {
+    return this.http.delete<ProcessoFase>(this.baseApiUrl + '/api/Sistema/delete-fase/' + id);
   }
 }
