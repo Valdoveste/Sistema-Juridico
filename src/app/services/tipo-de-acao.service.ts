c7928765de72fcf75e57e93f16dfb07e053dac43
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-05 01:26:07 -0300

    new service to TipoDeAcao

--- src/app/services/tipo-de-acao.service.ts
+++ src/app/services/tipo-de-acao.service.ts
@@ -14,6 +14,20 @@ export class TipoDeAcaoService {
   constructor(private http: HttpClient) { }
 
   getAllTipoDeAcao(): Observable<ProcessoTipoDeAcao[]> {
-    return this.http.get<ProcessoTipoDeAcao[]>(this.baseApiUrl + '/api/Sistema/ambito')
+    return this.http.get<ProcessoTipoDeAcao[]>(this.baseApiUrl + '/api/Sistema/tipo-de-acao');
+  }
+
+  createTipoDeAcao(createTipoDeAcaoRequest: ProcessoTipoDeAcao): Observable<ProcessoTipoDeAcao> {
+    delete createTipoDeAcaoRequest.ID;
+    return this.http.post<ProcessoTipoDeAcao>(this.baseApiUrl + '/api/Sistema/add-tipo-de-acao', createTipoDeAcaoRequest);
+  }
+
+  updateTipoDeAcao(id: string, updateTipoDeAcaoRequest: ProcessoTipoDeAcao): Observable<ProcessoTipoDeAcao> {
+    updateTipoDeAcaoRequest.ID = id;
+    return this.http.put<ProcessoTipoDeAcao>(this.baseApiUrl + '/api/Sistema/update-tipo-de-acao/' + id, updateTipoDeAcaoRequest);
+  }
+
+  deleteTipoDeAcao(id: string): Observable<ProcessoTipoDeAcao> {
+    return this.http.delete<ProcessoTipoDeAcao>(this.baseApiUrl + '/api/Sistema/delete-tipo-de-acao/' + id);
   }
 }
