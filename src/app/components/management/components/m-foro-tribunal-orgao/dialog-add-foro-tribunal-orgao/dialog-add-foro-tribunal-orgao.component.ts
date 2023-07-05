adee536fd75b071a8ba8ee31bed560c7c5c2b9b4
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-05 00:42:24 -0300

    FORO_TRIBUNAL_ORGAO Remove, Add, Edit dialog comp

--- src/app/components/management/components/m-foro-tribunal-orgao/dialog-add-foro-tribunal-orgao/dialog-add-foro-tribunal-orgao.component.ts
+++ src/app/components/management/components/m-foro-tribunal-orgao/dialog-add-foro-tribunal-orgao/dialog-add-foro-tribunal-orgao.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProcessoForoTribunalOrgao } from 'src/app/models/PROCESSO_FORO_TRIBUNAL_ORGAO.model';
+import { ForoTribunalOrgaoService } from 'src/app/services/foro-tribunal-orgao.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-foro-tribunal-orgao',
   templateUrl: './dialog-add-foro-tribunal-orgao.component.html',
   styleUrls: ['./dialog-add-foro-tribunal-orgao.component.scss']
 })
-export class DialogAddForoTribunalOrgaoComponent {
+export class DialogAddForoTribunalOrgaoComponent implements OnInit {
+  constructor(
+    private ForoTribunalOrgaoService: ForoTribunalOrgaoService,
+    public dialogRef: MatDialogRef<DialogAddForoTribunalOrgaoComponent>,
+  ) { }
 
+  createForoTribunalOrgaoForm!: FormGroup;
+
+  createForoTribunalOrgaoRequest: ProcessoForoTribunalOrgao = {
+    ID: '',
+    FORO_TRIBUNAL_ORGAO: ''
+  }
+
+  ngOnInit(): void {
+    this.createForoTribunalOrgaoForm = new FormGroup({
+      FORO_TRIBUNAL_ORGAO: new FormControl('', [Validators.required])
+    });
+  }
+
+  createForoTribunalOrgao() {
+    if (this.createForoTribunalOrgaoForm.valid) {
+      this.ForoTribunalOrgaoService.createForoTribunalOrgao(this.createForoTribunalOrgaoRequest)
+        .subscribe({
+          next: (response) => {
+            this.dialogRef.close(true);
+            this.createForoTribunalOrgaoForm.reset();
+          },
+          error: (response) => {
+            console.log(response)
+          }
+        });
+    }
+    return;
+  }
 }
--- src/app/components/management/components/m-foro-tribunal-orgao/m-foro-tribunal-orgao.component.ts
+++ src/app/components/management/components/m-foro-tribunal-orgao/m-foro-tribunal-orgao.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProcessoForoTribunalOrgao } from 'src/app/models/PROCESSO_FORO_TRIBUNAL_ORGAO.model';
+import { ForoTribunalOrgaoService } from 'src/app/services/foro-tribunal-orgao.service';
+
+import { DialogAddForoTribunalOrgaoComponent } from './dialog-add-foro-tribunal-orgao/dialog-add-foro-tribunal-orgao.component';
+import { DialogDeleteForoTribunalOrgaoComponent } from './dialog-delete-foro-tribunal-orgao/dialog-delete-foro-tribunal-orgao.component';
+import { DialogEditForoTribunalOrgaoComponent } from './dialog-edit-foro-tribunal-orgao/dialog-edit-foro-tribunal-orgao.component';
 
 @Component({
   selector: 'app-m-foro-tribunal-orgao',
   templateUrl: './m-foro-tribunal-orgao.component.html',
-  styleUrls: ['./m-foro-tribunal-orgao.component.scss']
+  styleUrls: ['./m-foro-tribunal-orgao.component.scss'],
 })
-export class MForoTribunalOrgaoComponent {
+export class MForoTribunalOrgaoComponent implements OnInit {
+  constructor(
+    public dialog: MatDialog,
+    private foroTribunalOrgaoService: ForoTribunalOrgaoService
+  ) { }
 
+  foroTribunalOrgaos: ProcessoForoTribunalOrgao[] = [];
+
+  ngOnInit(): void {
+    this.loadForoTribunalOrgaos();
+  }
+
+  loadForoTribunalOrgaos() {
+    this.foroTribunalOrgaoService.getAllForoTribunalOrgao()
+      .subscribe({
+        next: (foroTribunalOrgaos: any) => {
+          this.foroTribunalOrgaos = foroTribunalOrgaos;
+        },
+        error: (response: any) => {
+          console.log(response)
+        }
+      })
+  }
+
+  openDialogAdd(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    const dialogRefAdd = this.dialog.open(DialogAddForoTribunalOrgaoComponent, {
+      width: '350px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+    dialogRefAdd.afterClosed().subscribe(result => { result ? this.loadForoTribunalOrgaos() : null; })
+  }
+
+  openDialogRemove(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
+    const dialogRefRemove = this.dialog.open(DialogDeleteForoTribunalOrgaoComponent, {
+      width: '350px',
+      data: { value: (event.currentTarget as HTMLElement).previousElementSibling?.previousElementSibling?.innerHTML, id: id },
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+    dialogRefRemove.afterClosed().subscribe(result => { result ? this.loadForoTribunalOrgaos() : null; })
+  }
+
+  openDialogEdit(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
+    const dialogRefEdit = this.dialog.open(DialogEditForoTribunalOrgaoComponent, {
+      width: '350px',
+      data: { value: (event.currentTarget as HTMLElement).previousElementSibling?.innerHTML, id: id },
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+    dialogRefEdit.afterClosed().subscribe(result => { result ? this.loadForoTribunalOrgaos() : null; })
+  }
 }
