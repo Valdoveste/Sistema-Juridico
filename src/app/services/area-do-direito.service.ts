f83be1bca1dc22194b1c57e2d314edc1b66531ba
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-05 01:27:08 -0300

    CRUD Service to AreaDoDireito

--- src/app/services/area-do-direito.service.ts
+++ src/app/services/area-do-direito.service.ts
@@ -14,6 +14,20 @@ export class AreaDoDireitoService {
   constructor(private http: HttpClient) { }
 
   getAllAreaDoDireito(): Observable<ProcessoAreaDoDireito[]> {
-    return this.http.get<ProcessoAreaDoDireito[]>(this.baseApiUrl + '/api/Sistema/ambito')
+    return this.http.get<ProcessoAreaDoDireito[]>(this.baseApiUrl + '/api/Sistema/area-do-direito');
+  }
+
+  createAreaDoDireito(createAreaDoDireitoRequest: ProcessoAreaDoDireito): Observable<ProcessoAreaDoDireito> {
+    delete createAreaDoDireitoRequest.ID;
+    return this.http.post<ProcessoAreaDoDireito>(this.baseApiUrl + '/api/Sistema/add-area-do-direito', createAreaDoDireitoRequest);
+  }
+
+  updateAreaDoDireito(id: string, updateAreaDoDireitoRequest: ProcessoAreaDoDireito): Observable<ProcessoAreaDoDireito> {
+    updateAreaDoDireitoRequest.ID = id;
+    return this.http.put<ProcessoAreaDoDireito>(this.baseApiUrl + '/api/Sistema/update-area-do-direito/' + id, updateAreaDoDireitoRequest);
+  }
+
+  deleteAreaDoDireito(id: string): Observable<ProcessoAreaDoDireito> {
+    return this.http.delete<ProcessoAreaDoDireito>(this.baseApiUrl + '/api/Sistema/delete-area-do-direito/' + id);
   }
 }
