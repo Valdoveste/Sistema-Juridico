8ab5ac436f1f00ed7d57b2ba2dc66e504dbad055
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-15 01:22:53 -0300

    model and andamento

---
+++ src/app/models/PROCESSO_ANDAMENTO.model.ts
@@ -0,0 +1,9 @@
+export interface ProcessoAndamento {
+    ID?: String;
+    TIPO_ANDAMENTO: String;
+    DATA_ANDAMENTO: String;
+    ID_PROCESSO: String;
+    CRIADOR_ANDAMENTO: String;
+    DESC_ANDAMENTO: String;
+    DATA_CADASTRO: String;
+}
