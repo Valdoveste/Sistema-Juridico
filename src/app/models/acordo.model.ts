ce8d468c54a5cfe0db09c863178dcbed56edff22
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-15 00:21:08 -0300

    old model

--- src/app/models/acordo.model.ts
+++
@@ -1,9 +0,0 @@
-import { Data } from "@angular/router";
-
-export interface Acordo {
-  ID_ACORDO?: String;
-  ID_USUARIO: Number;
-  DATA_TENTATIVA: Data;
-  VALOR_TENTATIVA: Number;
-  CODICOES_TENTATIVA: String;
-}
