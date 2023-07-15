08104d7ae8e686ffdb2e8fec86541ac1b6cfc69b
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-15 00:20:48 -0300

    Movment base component

---
+++ src/app/components/processComponents/process-area/process-area-movement/dialog-edit-movement/dialog-edit-movement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-edit-movement',
+  templateUrl: './dialog-edit-movement.component.html',
+  styleUrls: ['./dialog-edit-movement.component.scss']
+})
+export class DialogEditMovementComponent {
+
+}
