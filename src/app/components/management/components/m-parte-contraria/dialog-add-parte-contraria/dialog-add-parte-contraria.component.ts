6083a128041d28b0610bad0d52cf2421bee5f5f7
Valdoveste <caio.valdoveste@outlook.com.br>
2023-07-26 22:33:37 -0300

    Parte Contraria Components, Service and Model

--- src/app/components/management/components/m-parte-contraria/dialog-add-parte-contraria/dialog-add-parte-contraria.component.ts
+++ src/app/components/management/components/m-parte-contraria/dialog-add-parte-contraria/dialog-add-parte-contraria.component.ts
@@ -12,10 +12,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class DialogAddParteContrariaComponent implements OnInit {
   constructor(
     private ParteContrariaService: ParteContrariaService,
-    public dialogRef: MatDialogRef<DialogAddParteContrariaComponent>,
+    public dialogRef: MatDialogRef<DialogAddParteContrariaComponent>
   ) { }
-
-  createParteContrariaForm!: FormGroup;
 
   createParteContrariaRequest: ProcessoParteContraria = {
     ID: '',
@@ -28,7 +26,7 @@ export class DialogAddParteContrariaComponent implements OnInit {
     RG: '',
     ENDERECO: '',
     CEP: '',
-    NUMERO: '',
+    NUMERO: 0,
     COMPLEMENTO: '',
     ESTADO: '',
     PAIS: '',
@@ -41,18 +39,50 @@ export class DialogAddParteContrariaComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.createParteContrariaForm = new FormGroup({
-      PARTECONTRARIA: new FormControl('', [Validators.required])
+    this.createParteContrariaFisicalForm = new FormGroup({
+      NOME: new FormControl('', [Validators.required]),
+      CPF: new FormControl('', [Validators.required]),
+      RG: new FormControl('', [Validators.required]),
+      ENDERECO: new FormControl('', [Validators.required]),
+      CEP: new FormControl('', [Validators.required]),
+      NUMERO: new FormControl(0, [Validators.required]),
+      COMPLEMENTO: new FormControl('', [Validators.required]),
+      CARGO: new FormControl('', [Validators.required]),
+      DATA_ADMISSAO: new FormControl('', [Validators.required]),
+      DATA_DEMISSAO: new FormControl('', [Validators.required]),
+      ULTIMO_SALARIO: new FormControl(0, [Validators.required]),
+    });
+
+    this.createParteContrariaLegalForm = new FormGroup({
+      NOME: new FormControl('', [Validators.required]),
+      NOME_FANTASIA: new FormControl('', [Validators.required]),
+      CNPJ: new FormControl('', [Validators.required]),
+      ENDERECO: new FormControl('', [Validators.required]),
+      CEP: new FormControl('', [Validators.required]),
+      NUMERO: new FormControl(0, [Validators.required]),
+      COMPLEMENTO: new FormControl('', [Validators.required]),
+      ESTADO: new FormControl('', [Validators.required]),
+      CIDADE: new FormControl('', [Validators.required]),
+      PAIS: new FormControl('', [Validators.required]),
+      OBSERVACAO: new FormControl('', [Validators.required]),
     });
   }
 
-  createParteContraria() {
-    if (this.createParteContrariaForm.valid) {
+  componentName: string = '';
+
+  swapTabs(componentName: string): void {
+    this.componentName = componentName;
+  }
+
+  createParteContrariaFisicalForm!: FormGroup;
+
+  createParteContrariaFisical() {
+    if (this.createParteContrariaFisicalForm.valid) {
+      this.createParteContrariaRequest.PF_PJ = 0;
       this.ParteContrariaService.createParteContraria(this.createParteContrariaRequest)
         .subscribe({
           next: (response) => {
             this.dialogRef.close(true);
-            this.createParteContrariaForm.reset();
           },
           error: (response) => {
             console.log(response)
@@ -60,5 +90,46 @@ export class DialogAddParteContrariaComponent implements OnInit {
         });
     }
     return;
+  }
+
+  onPersonRGChange() {
+    this.createParteContrariaRequest.RG = this.createParteContrariaRequest.RG!.replace(/\D/g, '');
+    this.createParteContrariaRequest.RG = this.createParteContrariaRequest.RG.replace(/(\d{2})(\d)/, '$1.$2');
+    this.createParteContrariaRequest.RG = this.createParteContrariaRequest.RG.replace(/(\d{3})(\d)/, '$1.$2');
+    this.createParteContrariaRequest.RG = this.createParteContrariaRequest.RG.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  }
+
+  onPersonCPFChange() {
+    this.createParteContrariaRequest.CPF = this.createParteContrariaRequest.CPF!.replace(/\D/g, '');
+    this.createParteContrariaRequest.CPF = this.createParteContrariaRequest.CPF.replace(/(\d{3})(\d)/, '$1.$2');
+    this.createParteContrariaRequest.CPF = this.createParteContrariaRequest.CPF.replace(/(\d{3})(\d)/, '$1.$2');
+    this.createParteContrariaRequest.CPF = this.createParteContrariaRequest.CPF.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  }
+
+
+  createParteContrariaLegalForm!: FormGroup;
+
+  createParteContrariaLegal() {
+    if (this.createParteContrariaLegalForm.valid) {
+      this.createParteContrariaRequest.PF_PJ = 1;
+      this.ParteContrariaService.createParteContraria(this.createParteContrariaRequest)
+        .subscribe({
+          next: (response) => {
+            this.dialogRef.close(true);
+          },
+          error: (response) => {
+            console.log(response)
+          }
+        });
+    }
+    return;
+  }
+
+  onPersonCNPJChange() {
+    this.createParteContrariaRequest.CNPJ = this.createParteContrariaRequest.CNPJ.replace(/\D/g, '');
+    this.createParteContrariaRequest.CNPJ = this.createParteContrariaRequest.CNPJ.replace(/(\d{2})(\d)/, '$1.$2');
+    this.createParteContrariaRequest.CNPJ = this.createParteContrariaRequest.CNPJ.replace(/(\d{3})(\d)/, '$1.$2');
+    this.createParteContrariaRequest.CNPJ = this.createParteContrariaRequest.CNPJ.replace(/(\d{3})(\d)/, '$1/$2');
+    this.createParteContrariaRequest.CNPJ = this.createParteContrariaRequest.CNPJ.replace(/(\d{4})(\d)/, '$1-$2');
   }
 }
