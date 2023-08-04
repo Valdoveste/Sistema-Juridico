d9a7f55a1670a0283f7843ac1f0d8a6d4bc5d017
Valdoveste <caio.valdoveste@outlook.com.br>
2023-08-03 23:27:22 -0300

    new component still on development

---
+++ src/app/components/management/components/m-parte-contraria/dialog-view-parte-contraria/dialog-view-parte-contraria.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-view-parte-contraria',
+  templateUrl: './dialog-view-parte-contraria.component.html',
+  styleUrls: ['./dialog-view-parte-contraria.component.scss']
+})
+export class DialogViewParteContrariaComponent {
+
+}
