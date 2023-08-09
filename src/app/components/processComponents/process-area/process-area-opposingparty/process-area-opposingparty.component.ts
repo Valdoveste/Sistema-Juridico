8480a182a22237b1fb1b5ced9f766eb1f981f178
Valdoveste <caio.valdoveste@outlook.com.br>
2023-08-09 00:46:07 -0300

    updating old component

--- src/app/components/processComponents/process-area/process-area-opposingparty/process-area-opposingparty.component.ts
+++ src/app/components/processComponents/process-area/process-area-opposingparty/process-area-opposingparty.component.ts
@@ -1,9 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { ProcessAreaComponent } from '../process-area.component';
+import { ParteContrariaService } from 'src/app/services/parte-contraria.service';
+import { ProcessoParteContraria } from 'src/app/models/PROCESSO_PARTE_CONTRARIA.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-process-area-opposingparty',
   templateUrl: './process-area-opposingparty.component.html',
   styleUrls: ['./process-area-opposingparty.component.scss']
 })
-export class ProcessAreaOpposingpartyComponent extends ProcessAreaComponent implements OnInit {}
+export class ProcessAreaOpposingpartyComponent implements OnInit {
+
+  constructor(
+    private ParteContrariaService: ParteContrariaService,
+    private route: ActivatedRoute,
+  ) { }
+
+  idProcesso: any;
+
+  componentName: number = 0;
+
+  swapTabs(componentName: number): void {
+    this.componentName = componentName;
+  }
+
+  viewParteContriaRequest: ProcessoParteContraria = {
+    ID: '',
+    ID_PROCESSO: '',
+    PF_PJ: 0,
+    NOME: '',
+    NOME_FANTASIA: '',
+    CPF: '',
+    CNPJ: '',
+    RG: '',
+    ENDERECO: '',
+    CEP: '',
+    NUMERO: 0,
+    COMPLEMENTO: '',
+    ESTADO: '',
+    PAIS: '',
+    CIDADE: '',
+    OBSERVACAO: '',
+    CARGO: '',
+    DATA_ADMISSAO: '',
+    DATA_DEMISSAO: '',
+    ULTIMO_SALARIO: 0,
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        this.idProcesso = params.get('id');
+        this.ParteContrariaService.getProcessoParteContraria(this.idProcesso)
+          .subscribe({
+            next: (response) => {
+              this.viewParteContriaRequest = response;
+              console.log(response)
+              this.componentName = response.PF_PJ;
+            },
+            error: (response) => {
+              console.log(response)
+            }
+          })
+      }
+    })
+
+  }
+}
