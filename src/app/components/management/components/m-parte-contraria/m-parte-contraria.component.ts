fcc0d8a5efb92ed26e79628e29ec78726cb12a5d
Valdoveste <caio.valdoveste@outlook.com.br>
2023-08-09 00:44:14 -0300

    removintg the querySelectorAll function

--- src/app/components/management/components/m-parte-contraria/m-parte-contraria.component.ts
+++ src/app/components/management/components/m-parte-contraria/m-parte-contraria.component.ts
@@ -47,10 +47,10 @@ export class MParteContrariaComponent implements OnInit {
     dialogRefAdd.afterClosed().subscribe(result => { result ? this.loadParteContrarias() : null; })
   }
 
-  openDialogRemove(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
+  openDialogRemove(enterAnimationDuration: string, exitAnimationDuration: string, id: String | undefined): void {
     const dialogRefRemove = this.dialog.open(DialogDeleteParteContrariaComponent, {
       width: '750px',
-      data: { value: document.getElementsByClassName(id as string), id: id },
+      data: { id: id },
       enterAnimationDuration,
       exitAnimationDuration,
     });
@@ -58,16 +58,10 @@ export class MParteContrariaComponent implements OnInit {
     dialogRefRemove.afterClosed().subscribe(result => { result ? this.loadParteContrarias() : null; })
   }
 
-  openDialogEdit(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
-    Array.from(document.getElementsByClassName(id as string)).forEach(
-      function (element, index, array) {
-        console.log(element.innerHTML)
-      }
-    )
-
+  openDialogEdit(enterAnimationDuration: string, exitAnimationDuration: string, id: String | undefined): void {
     const dialogRefEdit = this.dialog.open(DialogEditParteContriaComponent, {
       width: '750px',
-      data: { value: document.getElementsByClassName(id as string), id: id },
+      data: { id: id },
       enterAnimationDuration,
       exitAnimationDuration,
     });
@@ -75,11 +69,10 @@ export class MParteContrariaComponent implements OnInit {
     dialogRefEdit.afterClosed().subscribe(result => { result ? this.loadParteContrarias() : null; })
   }
 
-  openDialogView(enterAnimationDuration: string, exitAnimationDuration: string, event: Event, id: String | undefined): void {
+  openDialogView(enterAnimationDuration: string, exitAnimationDuration: string, id: String | undefined): void {
     const dialogRefEdit = this.dialog.open(DialogViewParteContrariaComponent, {
-      height: '600px',
       width: '750px',
-      data: { value: (event.currentTarget as HTMLElement).previousElementSibling?.innerHTML, id: id },
+      data: { id: id },
       enterAnimationDuration,
       exitAnimationDuration,
     });
