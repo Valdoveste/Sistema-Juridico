177a0ae21f6b28e59b9ad450203a54f91ecd2842
Valdoveste <caio.valdoveste@outlook.com.br>
2023-08-15 21:49:17 -0300

    Data now is only string instead of string | String

--- src/app/models/PROCESSO_ACORDO.model.ts
+++ src/app/models/PROCESSO_ACORDO.model.ts
@@ -1,7 +1,7 @@
 export interface ProcessoAcordo {
     ID?: string | String;
-    DATA_ACORDO: string | String;
-    VALOR_ACORDO: number;
+    DATA_ACORDO: string;
+    VALOR_ACORDO: string | number;
     ID_PROCESSO: string | String;
     CRIADOR_ACORDO: string | String;
     CONDICOES_TENTATIVA_DE_ACORDO: string | String;
