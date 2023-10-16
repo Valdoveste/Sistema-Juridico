c89831a079daf403e71287a4804380db171bad17
Valdoveste <caio.valdoveste@outlook.com.br>
2023-10-16 09:37:42 -0300

    New model

---
+++ SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Models/PROCESSO_LOG_ALTERACOES.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaJuridicoWebAPI.Models
+{
+  public class PROCESSO_LOG_ALTERACOES
+  {
+    [Key]
+    public Guid ID { get; set; }
+    public string ID_PROCESSO { get; set; }
+    public string ID_USUARIO { get; set; }
+    public string NOME_USUARIO { get; set; }
+    public string VALOR_ORIGINAL { get; set; }
+    public string VALOR_ATUAL { get; set; }
+    public string DATA_ALTERACAO { get; set; }
+  }
+}
