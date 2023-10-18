11f31dbff3d449190d3c3fd56f6d1c57736c491c
Valdoveste <caio.valdoveste@outlook.com.br>
2023-10-17 21:47:13 -0300

    Adding and log function after the process is updated

--- src/app/components/processComponents/process-edit/process-edit.component.ts
+++ src/app/components/processComponents/process-edit/process-edit.component.ts
@@ -1,30 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Processo } from 'src/app/models/PROCESSO.model';
 import { ProcessoService } from 'src/app/services/processo.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ProcessoPatronosAnteriors } from 'src/app/models/PROCESSO_PATRONOS_ANTERIORES.model';
+import { PatronoAnteriorService } from 'src/app/services/patrono-anterior.service';
+import { ProcessoLogAlteracoes } from 'src/app/models/PROCESSO_LOG_ALTERACOES.model';
+import { ProcessoLogAlteracoesService } from 'src/app/services/processo-log-alteracoes.service';
 
 @Component({
   selector: 'app-process-edit',
   templateUrl: './process-edit.component.html',
   styleUrls: ['./process-edit.component.scss']
 })
-export class ProcessEditComponent {
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private ProcessoService: ProcessoService
-  ) { }
-
-  updateProcessForm!: FormGroup;
-
-  componentName: string = '';
+export class ProcessEditComponent implements OnInit {
+  componentName: string | String = '';
 
   swapTabs(componentName: string): void {
     this.componentName = componentName;
   }
 
-  updateProcessRequest: Processo = {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private ProcessoService: ProcessoService,
+    private PatronoAnteriorService: PatronoAnteriorService,
+    private ProcessoLogAlteracoesService: ProcessoLogAlteracoesService
+  ) { }
+
+  updateProcessForm!: FormGroup;
+
+  @Input() updateProcessRequest: Processo = {
     ID_PROCESSO: '',
     NUMERO_PROCESSO: '',
     STATUS: '',
@@ -39,8 +46,8 @@ export class ProcessEditComponent {
     VARA: '',
     FORO_TRIBUNAL_ORGAO: '',
     FASE: '',
-    DATA_DISTRIBUICAO: new Date,
-    DATA_CITACAO: new Date,
+    DATA_DISTRIBUICAO: '',
+    DATA_CITACAO: '',
     PATRONO_RESPONSAVEL: '',
     PATRONOS_ANTERIORES: '',
     TEXTO_DO_OBJETO: '',
@@ -49,13 +56,30 @@ export class ProcessEditComponent {
     VALOR_INSTANCIA2: 0,
     VALOR_INSTANCIA3: 0,
     VALOR_INSTANCIA_EXTRAORDINARIA: 0,
-    DATA_CADASTRO_PROCESSO: new Date,
-    DATA_ULTIMO_ANDAMENTO: new Date,
-    DATA_ENCERRAMENTO: new Date,
+    DATA_CADASTRO_PROCESSO: '',
+    DATA_ULTIMO_ANDAMENTO: '',
+    DATA_ENCERRAMENTO: '',
     MOTIVO_ENCERRAMENTO: '',
     MOTIVO_BAIXA_PROVISORIA: ''
   }
 
+  createPatronoAnteriorRequest: ProcessoPatronosAnteriors = {
+    ID_PROCESSO: '',
+    ID_USUARIO: '',
+    NOME_USUARIO: '',
+    PATRONO_RESPONSAVEL_ATUAL: '',
+    PATRONO_RESPONSAVEL_CPF_CNPJ_ATUAL: '',
+    DATA_ALTERACAO: ''
+  }
+
+  createLogProcessoRequest: ProcessoLogAlteracoes = {
+    ID_PROCESSO: '',
+    ID_USUARIO: '',
+    NOME_USUARIO: '',
+    VALOR_ORIGINAL: '',
+    VALOR_ATUAL: '',
+    DATA_ALTERACAO: ''
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe({
@@ -66,19 +90,17 @@ export class ProcessEditComponent {
             next: (response) => {
               this.updateProcessRequest = response;
             },
-            error: (erroResponse) => {
-              console.log(erroResponse.error)
-            }
-          })
+            error: (err: HttpErrorResponse) => console.log(err)
+          });
         }
       }
-    })
+    });
 
     this.updateProcessForm = new FormGroup({
       // ID_PROCESSO: new FormControl(''),
+      // EMPRESA_CNPJ: new FormControl('', [Validators.required]),
       NUMERO_PROCESSO: new FormControl('', [Validators.required]),
       EMPRESA: new FormControl('', [Validators.required]),
-      // EMPRESA_CNPJ: new FormControl('', [Validators.required]),
       STATUS: new FormControl('', [Validators.required]),
       TIPO_DE_ACAO: new FormControl('', [Validators.required]),
       AREA_DO_DIREITO: new FormControl('', [Validators.required]),
@@ -112,20 +134,31 @@ export class ProcessEditComponent {
       next: (params) => {
         const id_processo = params.get('id')
         if (id_processo) {
-          this.ProcessoService.updateProcess(id_processo, this.updateProcessRequest).subscribe({
-            next: () => {
-              this.router.navigate(['/painel-processos', 'processo-detalhes', id_processo])
-            },
-            error: (errorResponse) => {
-              console.log(errorResponse)
-            }
-          })
+          this.ProcessoService.updateProcess(id_processo, this.updateProcessRequest)
+            .subscribe({
+              next: (response: any) => this.addLogAfterUpdateProcess(id_processo, response),
+
+              error: (err: HttpErrorResponse) => console.log(err)
+            });
         }
       },
-      error: (errorResponse) => {
-        console.log(errorResponse)
-      }
-    })
+      error: (err: HttpErrorResponse) => console.log(err)
+    });
   }
 
+  addLogAfterUpdateProcess(id_processo: string, response: any) {
+    for (let i = 0; i < response.length; i++) {
+      for (const propertyName in response[i]) {
+        if (response[i].hasOwnProperty(propertyName)) {
+          this.createLogProcessoRequest.VALOR_ORIGINAL = response[i][propertyName]['VALOR_ORIGINAL'].toString();
+          this.createLogProcessoRequest.VALOR_ATUAL = response[i][propertyName]['VALOR_ATUAL'].toString();
+
+          this.ProcessoLogAlteracoesService.addLogProcesso(id_processo, this.createLogProcessoRequest)
+            .subscribe({ error: (err: HttpErrorResponse) => console.log(err) });
+
+          this.router.navigate(['/painel-processos', 'processo-detalhes', id_processo]);
+        }
+      }
+    }
+  }
 }
