af4f86d39fcc70bdf7300b44c834f66f1e453511
Valdoveste <caio.valdoveste@outlook.com.br>
2023-10-19 08:30:07 -0300

    Changing the params type and adding new parameter "INPUT_ALTERADO"

--- src/app/models/PROCESSO_LOG_ALTERACOES.model.ts
+++ src/app/models/PROCESSO_LOG_ALTERACOES.model.ts
@@ -3,7 +3,8 @@ export interface ProcessoLogAlteracoes {
     ID_PROCESSO: string | String;
     ID_USUARIO: string | String;
     NOME_USUARIO: string | String;
-    VALOR_ORIGINAL: string | String;
-    VALOR_ATUAL: string | String;
+    VALOR_ORIGINAL: string | number;
+    VALOR_ATUAL: string | number;
+    INPUT_ALTERADO: string | String;
     DATA_ALTERACAO: string | String;
 }
