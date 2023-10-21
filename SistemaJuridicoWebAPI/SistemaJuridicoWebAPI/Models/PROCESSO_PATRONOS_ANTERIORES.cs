2b11fb07fb5d6a4c2b5cf69b147c6908e922cfbc
Valdoveste <caio.valdoveste@outlook.com.br>
2023-10-21 04:05:19 -0300

    Now it's only needed one parameter

--- SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Models/PROCESSO_PATRONOS_ANTERIORES.cs
+++ SistemaJuridicoWebAPI/SistemaJuridicoWebAPI/Models/PROCESSO_PATRONOS_ANTERIORES.cs
@@ -9,8 +9,7 @@ namespace SistemaJuridicoWebAPI.Models
     public string ID_PROCESSO { get; set; }
     public string ID_USUARIO { get; set; }
     public string NOME_USUARIO { get; set; }
-    public string PATRONO_RESPONSAVEL_ATUAL { get; set; }
-    public string PATRONO_RESPONSAVEL_CPF_CNPJ_ATUAL { get; set; }
+    public string PATRONO_RESPONSAVEL_ANTERIOR { get; set; }
     public string DATA_ALTERACAO { get; set; }
   }
 }
