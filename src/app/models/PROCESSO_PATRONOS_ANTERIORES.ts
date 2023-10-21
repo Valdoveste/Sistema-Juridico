269aa893eb582750a4e3d956bca7e19b75c465f8
Valdoveste <caio.valdoveste@outlook.com.br>
2023-10-21 04:05:51 -0300

    Importing the new renamed model

---
+++ src/app/models/PROCESSO_PATRONOS_ANTERIORES.ts
@@ -0,0 +1,8 @@
+export interface ProcessoPatronosAnteriors {
+    ID?: string | String;
+    ID_PROCESSO: string | String;
+    ID_USUARIO: string | String;
+    NOME_USUARIO: string | String;
+    PATRONO_RESPONSAVEL_ANTERIOR: string | String;
+    DATA_ALTERACAO: string | String;
+}
