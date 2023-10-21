8e980d58ef9c12611c28b5f3f3df2ff23fd3d7d4
Valdoveste <caio.valdoveste@outlook.com.br>
2023-10-21 03:52:52 -0300

    Issue #43 commits

--- src/app/components/processComponents/process-area/process-area-previous-patrons/process-area-previous-patrons.component.ts
+++ src/app/components/processComponents/process-area/process-area-previous-patrons/process-area-previous-patrons.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProcessoPatronosAnteriors } from 'src/app/models/PROCESSO_PATRONOS_ANTERIORES';
+import { PatronoAnteriorService } from 'src/app/services/patrono-anterior.service';
 
 @Component({
   selector: 'app-process-area-previous-patrons',
   templateUrl: './process-area-previous-patrons.component.html',
   styleUrls: ['./process-area-previous-patrons.component.scss']
 })
-export class ProcessAreaPreviousPatronsComponent {
+export class ProcessAreaPreviousPatronsComponent implements OnInit {
 
+  constructor(
+    private patronoAlteriorService: PatronoAnteriorService,
+    private ActivatedRouter: ActivatedRoute
+  ) { }
+
+  patronosAlteriores: ProcessoPatronosAnteriors[] = [];
+
+  ngOnInit(): void { this.loadPatronosAnteriores(); }
+
+  loadPatronosAnteriores() {
+    this.ActivatedRouter.paramMap
+      .subscribe({
+        next: (parms) => {
+          const ID_PROCESSO = parms.get('id');
+          if (ID_PROCESSO) {
+            this.patronoAlteriorService.getProcessoPatronoAnterior(ID_PROCESSO)
+              .subscribe({
+                next: (response) => {
+                  this.patronosAlteriores = response;
+                },
+                error: (err: HttpErrorResponse) => console.log(err)
+              });
+          }
+        }
+      });
+  }
 }
