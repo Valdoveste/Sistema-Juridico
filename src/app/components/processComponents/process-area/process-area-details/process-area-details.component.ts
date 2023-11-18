8644d5be3460d861b69337fb71816153ea22aa61
Valdoveste <caio.valdoveste@outlook.com.br>
2023-11-18 18:12:25 -0300

    load function

--- src/app/components/processComponents/process-area/process-area-details/process-area-details.component.ts
+++ src/app/components/processComponents/process-area/process-area-details/process-area-details.component.ts
@@ -1,9 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { ProcessAreaComponent } from '../process-area.component';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Processo } from 'src/app/models/PROCESSO.model';
 
 @Component({
   selector: 'app-process-area-details',
   templateUrl: './process-area-details.component.html',
   styleUrls: ['./process-area-details.component.scss']
 })
-export class ProcessAreaDetailsComponent extends ProcessAreaComponent implements OnInit {}
+export class ProcessAreaDetailsComponent extends ProcessAreaComponent implements OnInit {
+
+  processoDetalhes: Processo = {
+    ID_PROCESSO: '',
+    NUMERO_PROCESSO: '',
+    STATUS: '',
+    TIPO_DE_ACAO: '',
+    AREA_DO_DIREITO: '',
+    EMPRESA: '',
+    EMPRESA_CNPJ: '',
+    AMBITO: '',
+    ESTADO: '',
+    CIDADE: '',
+    PAIS: '',
+    VARA: '',
+    FORO_TRIBUNAL_ORGAO: '',
+    FASE: '',
+    DATA_DISTRIBUICAO: '',
+    DATA_CITACAO: '',
+    PARTE_CONTRARIA: '',
+    ID_PARTE_CONTRARIA: '',
+    PATRONO_RESPONSAVEL: '',
+    PATRONOS_ANTERIORES: '',
+    TEXTO_DO_OBJETO: '',
+    VALOR_DO_PEDIDO: 0,
+    VALOR_INSTANCIA1: 0,
+    VALOR_INSTANCIA2: 0,
+    VALOR_INSTANCIA3: 0,
+    VALOR_INSTANCIA_EXTRAORDINARIA: 0,
+    DATA_CADASTRO_PROCESSO: '',
+    DATA_ULTIMO_ANDAMENTO: '',
+    DATA_ENCERRAMENTO: '',
+    MOTIVO_ENCERRAMENTO: '',
+    MOTIVO_BAIXA_PROVISORIA: ''
+  };
+
+  ngOnInit(): void {
+    this.loadProcess();
+  }
+
+  loadProcess() {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+
+        if (id) {
+          this.processoService.getProcess(id)
+            .subscribe({
+              next: (response) => {
+                this.processoDetalhes = response;
+
+                if (response.STATUS == 'Encerrado' || response.STATUS == 'Baixa Provisória') {
+                  this.openDialogViewStatus('250ms', '100ms', response.STATUS, response.ID_PROCESSO!)
+                }
+              },
+              error: (err: HttpErrorResponse) => console.log(err)
+            });
+        }
+      },
+      error: (err: HttpErrorResponse) => console.log(err)
+    });
+  }
+}
